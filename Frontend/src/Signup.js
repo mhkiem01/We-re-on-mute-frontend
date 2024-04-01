01376b9d80d74ede1a714790e618f20ccbb3f8ee
Aanshu Patel <z5480653@ad.unsw.edu.au>
2024-04-01 16:48:06 +1100

    Reverting changes

--- Frontend/src/Signup.js
+++ Frontend/src/Signup.js
@@ -1,39 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Signup.css'; // Make sure this points to your CSS file
 
 function Signup() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch('/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          Name: `${firstName} ${lastName}`,
-          email, 
-          password,
-        }),
-      });
-
-      if (response.ok) {
-        const errorMessage = await response.json();
-        throw new Error(errorMessage.message)
-      }
-
-      console.log('Registration successful');
-    } catch (error) {
-      console.error('Registration failed', error.message);
-    }
-
-  }
 
   return (
     <div className="signup-container">
@@ -44,44 +12,20 @@ function Signup() {
       <div className="form-half">
         <div className="signup-form">
           <h2>Sign Up</h2>
-          <form onSubmit={handleSubmit}>
+          <form>
             <div className="form-group">
-              <input 
-              type="text" 
-              placeholder="First Name" 
-              className="input-field"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
+              <input type="text" placeholder="First Name" className="input-field"/>
             </div>
             <div className="form-group">
-              <input 
-              type="text" 
-              placeholder="Last Name" 
-              className="input-field"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            />
+              <input type="text" placeholder="Last Name" className="input-field"/>
             </div>
             <div className="form-group">
-              <input 
-              type="email" 
-              placeholder="Email Address" 
-              className="input-field"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+              <input type="email" placeholder="Email Address" className="input-field"/>
             </div>
             <div className="form-group">
-              <input 
-              type="password" 
-              placeholder="Create Password" 
-              className="input-field"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+              <input type="password" placeholder="Create Password" className="input-field"/>
             </div>
-            <button type="submit" className="signup-button">Create an account</button>
+            <a href="/" className="signup-button">Create an account</a>
           </form>
           <div className="login-section">
             <span>Already have an account? <a href="/" className="login-link">Log in</a></span>
