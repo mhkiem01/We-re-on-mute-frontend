5b336841fc0b3dccb034de8a6d0dfafc04ccef13
Muhammad Hakiem <z5468915@ad.unsw.edu.au>
2024-04-12 19:12:18 +1000

    Fix Linting

--- Backend/src/routes/sendFileInternally.ts
+++ Backend/src/routes/sendFileInternally.ts
@@ -5,20 +5,18 @@ import { validateFileRequest } from '../middleware/validateRequest';
 
 const router = express.Router();
 
-const upload = multer({ dest: 'uploads/' }); 
+const upload = multer({ dest: 'uploads/' });
 
 router.post('/', upload.single('file'), validateFileRequest, (req: Request, res: Response) => {
-  const { to, subject, body} = req.body;
+  const { to, subject, body } = req.body;
   const file = req.file;
-  console.log("file:", file);
   if (!file) {
     return res.status(400).json({ message: 'File upload failed' });
   }
 
   // const fileId = addFile(file.)
-  // const fileId = addFile(file.originalname, file.mimetype, file.path); 
-
-  // addNotification(fileId, body); 
+  // const fileId = addFile(file.originalname, file.mimetype, file.path);
+  // addNotification(fileId, body);
 
   res.status(200).json({ message: 'File sent internally and notification added successfully' });
 });
