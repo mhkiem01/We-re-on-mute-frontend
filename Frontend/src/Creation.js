b7dd3d78ee70fc95a3cce45fa19087754e92ce9a
Muhammad Hakiem <z5468915@ad.unsw.edu.au>
2024-04-14 17:08:54 +1000

    Nabihah Added Creation and Validation Page for frontend

---
+++ Frontend/src/Creation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Creation.css';
+
+const InvoiceCreation = ({ invoiceData }) => {
+  const renderArrayItems = (items) => {
+    return items.map((item, index) => <span key={index}>{item}</span>);
+  };
+
+  return (
+    <div className="creation-page">
+      <div className="creation-content">
+        <h1>Invoice Details</h1>
+        <div className="file-submit-container">
+          {Object.entries(invoiceData).map(([key, value]) => {
+            return (
+              <div className="invoice-field" key={key}>
+                <label>{key.charAt(0).toUpperCase() + key.slice(1)}:</label>
+                {Array.isArray(value) ? renderArrayItems(value) : <span>{value}</span>}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default InvoiceCreation;
